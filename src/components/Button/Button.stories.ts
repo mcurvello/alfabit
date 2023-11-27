f704b077ca5347dcd5000d10001a65eec975c2cc
Marcio Curvello <marciocurvello@gmail.com>
2023-11-27 13:10:19 -0300

    Add Button Component and Story

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,64 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import Button, { ButtonProps } from "./Button";
+
+const meta: Meta<ButtonProps> = {
+  title: "Molecules/Button",
+  component: Button,
+  argTypes: {
+    children: {
+      type: "string",
+    },
+    disabled: {
+      type: "boolean",
+    },
+    className: {
+      type: "string",
+    },
+  },
+};
+
+export default meta;
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+  },
+};
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    disabled: true,
+  },
+};
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    variant: "secondary",
+  },
+};
+
+export const SecondaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    variant: "secondary",
+    disabled: true,
+  },
+};
+
+export const Violet: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    className: "theme-violet",
+  },
+};
+
+export const VioletSecondary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    className: "theme-violet",
+    variant: "secondary",
+  },
+};
